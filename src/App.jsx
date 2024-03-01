43ab15bdfabc88f1666049375882b64a53e07057
vasanth134 <crazyvasanth004@gmail.com>
2024-03-01 20:00:07 +0530

    login form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import LoginPage from "./LoginPage"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <LoginPage />
   )
 }
